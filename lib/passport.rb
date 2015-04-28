1a1c228cdd626cbb015d304cf96b21ae07e79b5f
jessica engel & Cory Ribson <student@epicodus.com>
2015-04-28 10:38:25 -0700

    add a location, save it in a list of locations

--- lib/passport.rb
+++ lib/passport.rb
@@ -1,11 +1,10 @@
 class Passport
   @@our_passport = []
-  
+
   define_method(:initialize) do |location|
     @location = location
 
   end
-
 
   define_method(:places) do
     @location
@@ -13,6 +12,14 @@ class Passport
 
   define_singleton_method(:all_places) do
     @@our_passport
+  end
+
+  define_singleton_method(:clear) do
+    @@our_passport = []
+  end
+
+  define_method(:add_place) do
+    @@our_passport.push(self)
   end
 
 end
--- spec/passport_spec.rb
+++ spec/passport_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('passport')
 
 describe(Passport) do
+  before() do
+    Passport.clear()
+  end
 
   describe("#places") do
     it("retrieves the place input") do
@@ -9,10 +12,26 @@ describe(Passport) do
       expect(test_place.places()).to(eq("Zimbabwe"))
     end
   end
+
   describe(".all_places") do
     it("returns an empty array") do
       expect(Passport.all_places()).to(eq([]))
     end
-  end  
+  end
 
+  describe("#add_place") do
+    it("adds a location") do
+      test_place = Passport.new("Vancouver")
+      test_place.add_place()
+      expect(Passport.all_places()).to(eq([test_place]))
+    end
+  end
+
+  describe(".clear") do
+    it("washes away all of the places you've been to") do
+      Passport.new("Boston").add_place()
+      Passport.clear()
+      expect(Passport.all_places()).to(eq([]))
+    end
+  end
 end
